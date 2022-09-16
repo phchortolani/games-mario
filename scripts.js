d7f17ad3416e68f32e76060d1e2f9aba589d4d3f
phchortolani <phchortolani@gmail.com>
2022-09-15 22:59:33 -0300

    incluído o movimento do flappy bird

--- scripts.js
+++ scripts.js
@@ -1,35 +1,86 @@
 const mario = document.querySelector('.mario');
+const bird = document.querySelector('.bird');
+const view = document.querySelector(".view")
 const clouds = document.querySelector('.clouds');
 const pipe = document.querySelector('.pipe');
 const score = document.querySelectorAll('.score');
 const gameOverMsg = document.querySelector('.gameOverMsg');
-var scoreValue = 1;
-var controPointer = true;
-const keys = {
-    jump: "Space"
-}
 
-document.addEventListener('keydown', (e) => jump(e));
+var scoreValue = 1;
+var controlPointer = true;
+const keys = {
+    jump: "Space",
+    bird: {
+        up: "w",
+        right: "d",
+        down: "s",
+        left: "a"
+    }
+}
+var birdCoordinate = { y: 0, x: 0 }
+
+document.addEventListener('keydown', (e) => keyPress(e));
 document.addEventListener('click', (e) => jump({ code: keys.jump }));
 
-const jump = (e) => {
-    if (e.code == keys.jump && !mario.classList.contains("jump")) {
-        mario.classList.add("jump");
-        setTimeout(() => mario.classList.remove("jump"), 700)
-        controPointer = true;
+function keyPress(e) {
+    const distanceBirdToMove = 10;
+    if (e.code == keys.jump && !mario.classList.contains("jump")) return jump(e)
+
+    if (e.key == keys.bird.up) {
+        birdCoordinate.y -= distanceBirdToMove;
     }
+    if (e.key == keys.bird.right) {
+        birdCoordinate.x += distanceBirdToMove;
+
+    }
+    if (e.key == keys.bird.down) {
+        birdCoordinate.y += distanceBirdToMove;
+    }
+    if (e.key == keys.bird.left) {
+        birdCoordinate.x -= distanceBirdToMove;
+    }
+
+    limitarPassarinho();
+
+    document.documentElement.style.setProperty('--bird-y', `${birdCoordinate.y}%`)
+    document.documentElement.style.setProperty('--bird-x', `${birdCoordinate.x}%`)
+}
+
+
+function limitarPassarinho() {
+
+    if (birdCoordinate.x > 90) {
+        birdCoordinate.x = 95;
+    }
+    if (birdCoordinate.y > 85) {
+        birdCoordinate.y = 80;
+    }
+    if (birdCoordinate.y < 0) {
+        birdCoordinate.y = 0;
+    }
+    if (birdCoordinate.x < 0) {
+        birdCoordinate.x = 0;
+    }
+}
+
+
+const jump = (e) => {
+    mario.classList.add("jump");
+    setTimeout(() => mario.classList.remove("jump"), 700)
+    controlPointer = true;
 
 }
 
 const HasRunning = setInterval(() => {
-
     const pipePosition = pipe.offsetLeft
     const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
-
+    //mario automatico
+    if (pipePosition < 300 ) {
+        jump({ code: keys.jump })
+    };
     if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 75) {
         endGame(marioPosition, pipePosition);
     }
-
     if (pipe.offsetLeft < 0) {
         pontuar()
     }
@@ -41,8 +92,8 @@ function reset() {
 }
 
 function pontuar() {
-    if (controPointer) {
-        controPointer = false;
+    if (controlPointer) {
+        controlPointer = false;
         score.forEach((e) => e.innerHTML = scoreValue)
         scoreValue++;
     }
